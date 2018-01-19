af00d3fd081c53bc73d5655ed49f721379ca2200
zxj <zxj@zxjs-MacBook-Pro-7.local>
2018-01-19 10:35:16 +0800

    add future test

--- src/main/java/com/jdk9/demo/interfaceEx/impl/StudentServiceImpl.java
+++ src/main/java/com/jdk9/demo/interfaceEx/impl/StudentServiceImpl.java
@@ -4,11 +4,11 @@ import com.jdk9.demo.interfaceEx.StudentService;
 
 public class StudentServiceImpl implements StudentService{
 
-
     public static void main(String[] args) {
 
         StudentServiceImpl studentService = new StudentServiceImpl();
 
         studentService.getStudent1();
+
     }
 }
