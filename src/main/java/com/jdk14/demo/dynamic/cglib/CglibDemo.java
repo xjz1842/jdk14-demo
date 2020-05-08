abbc767bd4611cb0109339422be2fb1e8ed06bd1
zxj <zhanxuejun@unisound.com>
2020-05-08 21:33:15 +0800

    update jdk proxy

--- src/main/java/com/jdk14/demo/dynamic/cglib/CglibDemo.java
+++ src/main/java/com/jdk14/demo/dynamic/cglib/CglibDemo.java
@@ -2,7 +2,6 @@ package com.jdk14.demo.dynamic.cglib;
 
 
 import com.jdk14.demo.dynamic.jdk.Zhangsan;
-import net.sf.cglib.proxy.Callback;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
@@ -11,11 +10,9 @@ import java.lang.reflect.Method;
 
 public class CglibDemo {
 
-
     public static void main(String[] args) {
 
         Enhancer  enhancer = new Enhancer();
-
         enhancer.setSuperclass(Zhangsan.class);
 
         enhancer.setCallback(new MethodInterceptor() {
@@ -23,7 +20,6 @@ public class CglibDemo {
             public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
 
                 System.out.println(method);
-
                 System.out.println("before");
                 Object res = methodProxy.invokeSuper(o, args);
                 System.out.println("after");
@@ -34,6 +30,7 @@ public class CglibDemo {
         Zhangsan zhangsan = (Zhangsan) enhancer.create();
 
         zhangsan.findObject();
+
     }
 
 }
--- src/main/java/com/jdk14/demo/dynamic/jdk/MyClassLoader.java
+++ src/main/java/com/jdk14/demo/dynamic/jdk/MyClassLoader.java
@@ -5,17 +5,17 @@ import java.io.*;
 /**
  * @author xjz
  */
-public class MyClassLoader extends ClassLoader{
+public class MyClassLoader extends ClassLoader {
 
     private File dir;
 
     private String proxyClassPackage;
 
-    public String getProxyClassPackage(){
+    public String getProxyClassPackage() {
         return proxyClassPackage;
     }
 
-    public File getDir(){
+    public File getDir() {
         return dir;
     }
 
@@ -27,15 +27,15 @@ public class MyClassLoader extends ClassLoader{
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
 
-        if(dir != null){
-            File classFile = new File(dir,name+".class");
+        if (dir != null) {
+            File classFile = new File(dir, name + ".class");
 
-            if(classFile.exists()){
-                try{
-                 byte[] classBytes =  copyToByteArray(classFile);
+            if (classFile.exists()) {
+                try {
+                    byte[] classBytes = copyToByteArray(classFile);
 
-                 return defineClass(proxyClassPackage+"."+name,classBytes,0,classBytes.length);
-                }catch (IOException e){
+                    return defineClass(proxyClassPackage + "." + name, classBytes, 0, classBytes.length);
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
@@ -43,18 +43,37 @@ public class MyClassLoader extends ClassLoader{
         return super.findClass(name);
     }
 
-    private byte[] copyToByteArray(File file)throws IOException{
-        FileInputStream fileInputStream  = new FileInputStream(file);
-        BufferedReader bufferedInputStream = new BufferedReader(new InputStreamReader(fileInputStream));
+    private byte[] copyToByteArray(File file) throws IOException {
 
-        String line;
-        StringBuilder stringBuilder = new StringBuilder();
+        InputStream inputStream = new FileInputStream(file);
 
-        while((line=bufferedInputStream.readLine()) != null){
-            stringBuilder.append(line);
+        byte[] result;
+        try(ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            byte[] data = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(data, 0, data.length)) != -1) {
+                outputStream.write(data, 0, bytesRead);
+            }
+            result = outputStream.toByteArray();
+        }finally {
+            inputStream.close();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        InputStream inputStream = new FileInputStream("/Users/zxj/github/jdk14-demo/src/main/java/com/jdk14/demo/dynamic/jdk/$MyProxy0.class");
+
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            byte[] data = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(data, 0, data.length)) != -1) {
+                outputStream.write(data, 0, bytesRead);
+            }
+        } finally {
+            inputStream.close();
         }
 
-        bufferedInputStream.close();
-        return stringBuilder.toString().getBytes();
     }
 }
